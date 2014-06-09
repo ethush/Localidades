d649d87fd3fb4cafb99397de8da110410c9cc2ad
Raziel Ventura <ethush@gmail.com>
2014-06-09 15:57:15 -0500

    More bug fixes
    Validaciones extra para servicios de localización antes de iniciar la
    aplicación y opción de salida.
    
    - Pendiente agregar funciones de rendimiento y estabilidad.

--- src/com/digepo/localidades/LunchActivity.java
+++ src/com/digepo/localidades/LunchActivity.java
@@ -10,8 +10,10 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
@@ -27,6 +29,9 @@ public class LunchActivity extends Activity {
 	boolean isWifi;
 		
 	boolean isbackPressed;
+	boolean second;
+	boolean first;
+	
 	
 	LocationManager locManager = null;
 	/* (non-Javadoc)
@@ -88,45 +93,57 @@ public class LunchActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 		if(isbackPressed) {
-			
 			Log.e("onResume", "iniciaApp");
 			iniciaApp();
+			if (first) first = false;
 		}
 		else {
-			
-			Log.e("Todo falso","Booleanas negativas");
-			
 			isbackPressed = false;
-			
+			first = false;
 		}
 	}
 	
 	@Override
+	protected void onRestart() {
+		super.onRestart();
+		isbackPressed = true;
+		first = true;
+	}
+	
+	@Override
 	public void onBackPressed() {
-		// TODO Auto-generated method stub
 		super.onBackPressed();
 		Log.e("onBackPressed", "Back");
 	}
 	
 	public void iniciaApp() {
+		boolean serviceExists = false;
 		
 		if(locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
 			Log.e("Iniciando", "GPSservice activo");
 			isGPS = true;
+			
+			serviceExists = true;
 		}
 		else if(locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
 			Log.e("Iniciando", "WIFIservice activo");
 			isWifi = true;
+			
+			serviceExists = true;
 		}
-		else {
+		
+		if(!serviceExists && !first) {
 			
 			Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS); 
 	        startActivity(intent);
 			Toast.makeText(getApplicationContext(), "No se ha encontrado un proveedor de localización activo.", Toast.LENGTH_LONG).show();
 		}
 		
+		
+		
+		
 		if (isGPS || isWifi) {
-			Log.e("Iniciando", "Lanzando App");
+			Log.e("Funcion", "Lanzando App");
 			TimerTask task = new TimerTask() {
 				/* Timer para definir tiempo de espera antes de lanzar la pantalla principal. */
 				@Override
@@ -142,8 +159,40 @@ public class LunchActivity extends Activity {
 			Timer timer = new Timer();
 			timer.schedule(task, 3000);
 		}
-		else {
-			Log.e("Error", "No hay servicios");
+		
+		if(!serviceExists && !first) {
+			AlertDialog.Builder alert = new AlertDialog.Builder(this);
+			alert.setMessage("No se habilitaron servicios de localización WIFI o GPS. \n ¿Desea continuar?");
+			alert.setCancelable(false);
+			
+			alert.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					Intent intent = new Intent().setClass(LunchActivity.this, MainActivity.class);
+					startActivity(intent);
+					overridePendingTransition(R.anim.zoom_splash_out, R.anim.zoom_splash);
+					finish();
+				}
+				
+			});
+			
+			alert.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();
+					finish();
+				}
+			});
+			
+			
+			AlertDialog popup = alert.create();
+			popup.getCurrentFocus();
+			popup.show();
+			
 		}
+		
 	}
+	
 }
